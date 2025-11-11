daef3c1ee59d6661b035fb0b9237fae779512e3c
Aditi <145008599+aditii-s@users.noreply.github.com>
2025-11-11 20:05:05 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom' // 1. Import
-import './index.css'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import FitnessBuddy from "./App";
+import "./index.css"; // Tailwind included
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <BrowserRouter> {/* 2. Wrap App */}
-      <App />
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <FitnessBuddy />
     </BrowserRouter>
-  </StrictMode>,
-)
+  </React.StrictMode>
+);
